4410059b7c96ca5e769c6e5f09e773c2c31f235e
BauDahn <bdahcos@upv.edu.es>
2026-02-27 09:48:30 +0100

    Delete sistema.py

--- sistema.py
+++
@@ -1,6 +0,0 @@
-'''
-Archivo donde se programaran las funciones que se utilizan en el main para tratar la base de datos, para hacer el modelo y para evaluarlo.
-'''
-
-def limpiarDataFrame():
-    pass
